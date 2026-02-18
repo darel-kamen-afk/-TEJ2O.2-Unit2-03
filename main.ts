b8061b6604c53129fcc9c2b20e4a2bf8530a6bd1
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-02-17 20:49:56 -0500

    I added: The area and perimeter of a rectangle

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
-*/
+// 1. Setup variables
+let length = 5
+let width = 3
 
-basic.showString('Hello, World!')
+// 2. Perform the actual math
+let area = length * width
+let perimeter = 2 * (length + width)
+
+// 3. Show the Area math: "5 * 3 = 15"
+// Then show the final answer with units
+basic.showString(length + " * " + width + " = " + area)
+basic.showString("Area: " + area + " cm^2")
+
+basic.pause(1000) // Pause for 1 second
+
+// 4. Show the Perimeter math: "2 * (5 + 3) = 16"
+// Then show the final answer with units
+basic.showString("2 * (" + length + " + " + width + ") = " + perimeter)
+basic.showString("Perimeter: " + perimeter + " cm")
